d48008418880b1c02be8261c902a76a15b5b5290
basil <basil1987@gmail.com>
2023-05-08 12:50:24 +0530

    I have changed the UI message to user

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
             <MuiThemeProvider>
                 <div className="centerize">
                     <Paper zDepth={1} className="content">
-                        <h2>Hi Everyone, Type something</h2>
+                        <h2>Please type a message</h2>
                         <TextField ref={ref => this.textField = ref} onKeyUp={this.onEnterPress.bind(this)}
                                    hintText="Type your sentence."/>
                         <RaisedButton  label="Send" style={style} onClick={this.analyzeSentence.bind(this)}/>
