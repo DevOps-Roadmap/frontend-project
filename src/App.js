66c478e3615a2a34932fcd014f628648fe2bf730
basil1987 <basil1987@gmail.com>
2023-12-13 08:43:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://54.163.98.246:8080/sentiment', {
+        fetch('http://54.163.98.10:8080/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
