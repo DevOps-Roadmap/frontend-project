e09cf55e9606804a4c15466b5bf798067e2b70e4
Ajay john <107482384+ajuzajay@users.noreply.github.com>
2024-02-22 22:12:38 +0530

    Update update-manifest.sh

--- update-manifest.sh
+++ update-manifest.sh
@@ -7,8 +7,14 @@ git clone https://github.com/ajuzajay/frontend-project.git /tmp/temp_repo
 
 cd /tmp/temp_repo
 
+# Set Git identity
+git config --global user.email "ajay95264@gmail.com"
+git config --global user.name "ajuzajay"
+
+# Replace '***' with the actual tag or commit ID
 sed -i "s|image:.*|image: ajayjohn100/frontend-project:$2:$3|g" k8s/simple/frontend-deployment.yaml
 
 git add .
 git commit -m "update manifest"
+
 git push
